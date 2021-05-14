45111a98eb8327134c8ca331ea0263548ac83d63
Happyrobot33 <32679887+Happyrobot33@users.noreply.github.com>
2021-05-14 15:10:46 -0400

    Clean up code format

--- Code/Monorail_Controller/Monorail_Controller.ino
+++ Code/Monorail_Controller/Monorail_Controller.ino
@@ -15,16 +15,16 @@ int stepper1endstop = 34;
 int stepper2endstop = 35;
 int stepper3endstop = 36;
 
-const int STEPPER_SHIFT_ANGLE = 70; //this is the angle that the stepper mount is shifted by for vertical movement
+const int STEPPER_SHIFT_ANGLE = 70;	//this is the angle that the stepper mount is shifted by for vertical movement
 const int MAX_SPEED = 400;
 const int MAX_ACCEL = MAX_SPEED * 3;
-const float MM_TO_STEP_RATIO = -0.75; //This is a negative to reverse the direction of all three steppers easily
-const int ZERO_SPEED = 100; //how quickly should we zero the robot for initial position reset
-const int PROGRAM_LINE_COUNT = 100; //whats the maximum ammount of lines the program can be
-String programList[PROGRAM_LINE_COUNT]; //maximum of 100 commands (changeable ofc, dynamic arrays arent a thing I wanted to implement here)
-int lastArrayPoint = 0; //used to know where we are in the listed program
-int programCounter = 0; //used to count what line we are on in the program
-float percentSpeed = 1.0f;
+const float MM_TO_STEP_RATIO = -0.75;	//This is a negative to reverse the direction of all three steppers easily
+const int ZERO_SPEED = 100;	//how quickly should we zero the robot for initial position reset
+const int PROGRAM_LINE_COUNT = 100;	//whats the maximum ammount of lines the program can be
+String programList[PROGRAM_LINE_COUNT];	//maximum of 100 commands (changeable ofc, dynamic arrays arent a thing I wanted to implement here)
+int lastArrayPoint = 0;	//used to know where we are in the listed program
+int programCounter = 0;	//used to count what line we are on in the program
+float percentSpeed = 1.0 f;
 
 void setMotionAccel(int accel = MAX_ACCEL);
 
@@ -88,226 +88,263 @@ jmove(100,0,0);\
 ";
 
 //This takes the program string and splits it into commands per line, putting it into programList
-void splitProgram(){
-  while(program.indexOf(';') != -1){
-    programList[lastArrayPoint] = program.substring(0, program.indexOf(';'));
-    program = program.substring(program.indexOf(';') + 1, program.length());
-    lastArrayPoint++;
-  }
+void splitProgram()
+{
+	while (program.indexOf(';') != -1)
+	{
+		programList[lastArrayPoint] = program.substring(0, program.indexOf(';'));
+		program = program.substring(program.indexOf(';') + 1, program.length());
+		lastArrayPoint++;
+	}
 }
 
-void stringToArray(String * ar, String inputStr){
-  int r=0, t=0;
-  
-  for (int i=0; i < inputStr.length(); i++)
-  { 
-   if(inputStr.charAt(i) == ',' || inputStr.charAt(i) == ')') 
-    { 
-      ar[t] = inputStr.substring(r, i); 
-      r=(i+1); 
-      t++;
-    }
-  }
+void stringToArray(String *ar, String inputStr)
+{
+	int r = 0, t = 0;
+
+	for (int i = 0; i < inputStr.length(); i++)
+	{
+		if (inputStr.charAt(i) == ',' || inputStr.charAt(i) == ')')
+		{
+			ar[t] = inputStr.substring(r, i);
+			r = (i + 1);
+			t++;
+		}
+	}
 }
 
 //This takes a single line command and figures out what it means and where to send input values, if any
-void parseCommand(String command){
-  //Comment Handling
-  if(command.indexOf("#") != -1){
-    //do nothing :)
-    return;
-  }
-  //Motion Commands
-  else if(command.indexOf("lmove") != -1){
-    //Serial.println(command);
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[3];
-    stringToArray(values, valueset);
-    int x = values[0].toInt();
-    int y = values[1].toInt();
-    int z = values[2].toInt();
-    lmoveToCoordinates(x,y,z);
-  }
-  else if(command.indexOf("jmove") != -1){
-    //Serial.println(command);
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[3];
-    stringToArray(values, valueset);
-    int x = values[0].toInt();
-    int y = values[1].toInt();
-    int z = values[2].toInt();
-    jmoveToCoordinates(x,y,z);
-  }
-  else if(command.indexOf("CCarc") != -1){
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[6];
-    stringToArray(values, valueset);
-    int x      = values[0].toInt();
-    int y      = values[1].toInt();
-    int z      = values[2].toInt();
-    int r      = values[3].toInt();
-    int sangle = values[4].toInt();
-    int eangle = values[5].toInt();
-    arcmoveCC(x,y,z,r,sangle,eangle);
-  }
-  else if(command.indexOf("Carc") != -1){
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[6];
-    stringToArray(values, valueset);
-    int x      = values[0].toInt();
-    int y      = values[1].toInt();
-    int z      = values[2].toInt();
-    int r      = values[3].toInt();
-    int sangle = values[4].toInt();
-    int eangle = values[5].toInt();
-    arcmoveC(x,y,z,r,sangle,eangle);
-  }
-  else if(command.indexOf("speed()") != -1){
-    //Serial.println(command);
-    percentSpeed = 1.0;
-  }
-  else if(command.indexOf("speed") != -1){
-    //Serial.println(command);
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[1];
-    stringToArray(values, valueset);
-    percentSpeed = values[0].toInt() / 100.0;
-  }
-  else if(command.indexOf("accel()") != -1){
-    //Serial.println(command);
-    setMotionAccel(MAX_ACCEL); //Reset motion accel to max
-  }
-  else if(command.indexOf("accel") != -1){
-    //Serial.println(command);
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[1];
-    stringToArray(values, valueset);
-    setMotionAccel((values[0].toInt() / 100.0) * MAX_ACCEL); //find the value in the command, then divide it by 100 and multiply it by MAX_ACCEL
-  }
+void parseCommand(String command)
+{
+	//Comment Handling
+	if (command.indexOf("#") != -1)
+	{
+		//do nothing :)
+		return;
+	}
 
-  //Time Commands
-  if(command.indexOf("delay") != -1){
-    //Serial.println(command);
-    String valueset = command.substring(command.indexOf("(") + 1);
-    String values[1];
-    stringToArray(values, valueset);
-    delay(values[0].toInt());
-  }
+	//Motion Commands
+	else if (command.indexOf("lmove") != -1)
+	{
+		//Serial.println(command);
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[3];
+		stringToArray(values, valueset);
+		int x = values[0].toInt();
+		int y = values[1].toInt();
+		int z = values[2].toInt();
+		lmoveToCoordinates(x, y, z);
+	}
+	else if (command.indexOf("jmove") != -1)
+	{
+		//Serial.println(command);
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[3];
+		stringToArray(values, valueset);
+		int x = values[0].toInt();
+		int y = values[1].toInt();
+		int z = values[2].toInt();
+		jmoveToCoordinates(x, y, z);
+	}
+	else if (command.indexOf("CCarc") != -1)
+	{
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[6];
+		stringToArray(values, valueset);
+		int x = values[0].toInt();
+		int y = values[1].toInt();
+		int z = values[2].toInt();
+		int r = values[3].toInt();
+		int sangle = values[4].toInt();
+		int eangle = values[5].toInt();
+		arcmoveCC(x, y, z, r, sangle, eangle);
+	}
+	else if (command.indexOf("Carc") != -1)
+	{
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[6];
+		stringToArray(values, valueset);
+		int x = values[0].toInt();
+		int y = values[1].toInt();
+		int z = values[2].toInt();
+		int r = values[3].toInt();
+		int sangle = values[4].toInt();
+		int eangle = values[5].toInt();
+		arcmoveC(x, y, z, r, sangle, eangle);
+	}
+	else if (command.indexOf("speed()") != -1)
+	{
+		//Serial.println(command);
+		percentSpeed = 1.0;
+	}
+	else if (command.indexOf("speed") != -1)
+	{
+		//Serial.println(command);
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[1];
+		stringToArray(values, valueset);
+		percentSpeed = values[0].toInt() / 100.0;
+	}
+	else if (command.indexOf("accel()") != -1)
+	{
+		//Serial.println(command);
+		setMotionAccel(MAX_ACCEL);	//Reset motion accel to max
+	}
+	else if (command.indexOf("accel") != -1)
+	{
+		//Serial.println(command);
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[1];
+		stringToArray(values, valueset);
+		setMotionAccel((values[0].toInt() / 100.0) *MAX_ACCEL);	//find the value in the command, then divide it by 100 and multiply it by MAX_ACCEL
+	}
+
+	//Time Commands
+	if (command.indexOf("delay") != -1)
+	{
+		//Serial.println(command);
+		String valueset = command.substring(command.indexOf("(") + 1);
+		String values[1];
+		stringToArray(values, valueset);
+		delay(values[0].toInt());
+	}
 }
 
 void setup()
 {
-    pinMode(stepper1endstop, INPUT);
-    pinMode(stepper2endstop, INPUT);
-    pinMode(stepper3endstop, INPUT);
-    setMotionAccel();
-    //Serial.begin(9600);
-    splitProgram();
-    //Initial test move
-    reZero();
-    jmoveToCoordinates(0,0,0);
+	pinMode(stepper1endstop, INPUT);
+	pinMode(stepper2endstop, INPUT);
+	pinMode(stepper3endstop, INPUT);
+	setMotionAccel();
+	//Serial.begin(9600);
+	splitProgram();
+	//Initial test move
+	reZero();
+	jmoveToCoordinates(0, 0, 0);
 }
 
 void loop()
 {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
+	stepper1.run();
+	stepper2.run();
+	stepper3.run();
 
-    //if we have reached the position we are trying to move to, we have finished a line and should move to the next line in the program
-    if(stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0 && stepper3.distanceToGo() == 0){
-      parseCommand(programList[programCounter]);
-      programCounter++;
-    }
+	//if we have reached the position we are trying to move to, we have finished a line and should move to the next line in the program
+	if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0 && stepper3.distanceToGo() == 0)
+	{
+		parseCommand(programList[programCounter]);
+		programCounter++;
+	}
 
-    //if we reach the end of the program, go into an infinite loop to stop execution
-    if(programCounter == PROGRAM_LINE_COUNT && true){
-      //Serial.println("Program Complete!");
-      //while(true); //stop execution by getting stuck in a infinite loop
-      programCounter = 0;
-    }
+	//if we reach the end of the program, go into an infinite loop to stop execution
+	if (programCounter == PROGRAM_LINE_COUNT && true)
+	{
+		//Serial.println("Program Complete!");
+		//while(true);	//stop execution by getting stuck in a infinite loop
+		programCounter = 0;
+	}
 }
 
-void moveToCoordinates(float x, float y, float z){
-  //initial offsets to cart position offsets
-  //x = x + 140;
-  //y = y + 180;
-  //z = 580 - z;
-  
-  stepper1.moveTo((x - z) / MM_TO_STEP_RATIO);
-  stepper2.moveTo((x - (y * tan(radians(90 - STEPPER_SHIFT_ANGLE)))) / MM_TO_STEP_RATIO);
-  stepper3.moveTo((x + z) / MM_TO_STEP_RATIO);
+void moveToCoordinates(float x, float y, float z)
+{
+	//initial offsets to cart position offsets
+	//x = x + 140;
+	//y = y + 180;
+	//z = 580 - z;
+
+	stepper1.moveTo((x - z) / MM_TO_STEP_RATIO);
+	stepper2.moveTo((x - (y* tan(radians(90 - STEPPER_SHIFT_ANGLE)))) / MM_TO_STEP_RATIO);
+	stepper3.moveTo((x + z) / MM_TO_STEP_RATIO);
 }
 
-void lmoveToCoordinates(float x, float y, float z){
-  moveToCoordinates(x,y,z);
-  //abs all of these as we just want the distance, not the direction
-  long s1d = abs(stepper1.distanceToGo());
-  long s2d = abs(stepper2.distanceToGo());
-  long s3d = abs(stepper3.distanceToGo());
-  float MaxDistance = max(s1d, s3d);
-  
-  float VectorMultiplier = 1.0f / MaxDistance;
-  if(s1d == 0 && s3d == 0) {VectorMultiplier = 1.0f / s2d;} //account for the fact that if both exterior carriages dont move, we will calc NaN for vector multiplier
+void lmoveToCoordinates(float x, float y, float z)
+{
+	moveToCoordinates(x, y, z);
+	//abs all of these as we just want the distance, not the direction
+	long s1d = abs(stepper1.distanceToGo());
+	long s2d = abs(stepper2.distanceToGo());
+	long s3d = abs(stepper3.distanceToGo());
+	float MaxDistance = max(s1d, s3d);
 
-  stepper1.setMaxSpeed((VectorMultiplier * s1d) * MAX_SPEED * percentSpeed);
-  stepper2.setMaxSpeed((VectorMultiplier * s2d) * MAX_SPEED * percentSpeed);
-  stepper3.setMaxSpeed((VectorMultiplier * s3d) * MAX_SPEED * percentSpeed);
+	float VectorMultiplier = 1.0 f / MaxDistance;
+	if (s1d == 0 && s3d == 0) { VectorMultiplier = 1.0 f / s2d; }	//account for the fact that if both exterior carriages dont move, we will calc NaN for vector multiplier
+
+	stepper1.setMaxSpeed((VectorMultiplier *s1d) *MAX_SPEED *percentSpeed);
+	stepper2.setMaxSpeed((VectorMultiplier *s2d) *MAX_SPEED *percentSpeed);
+	stepper3.setMaxSpeed((VectorMultiplier *s3d) *MAX_SPEED *percentSpeed);
 }
 
-void jmoveToCoordinates(float x, float y, float z){
-  moveToCoordinates(x,y,z);
-  stepper1.setMaxSpeed(MAX_SPEED * percentSpeed);
-  stepper2.setMaxSpeed(MAX_SPEED * percentSpeed);
-  stepper3.setMaxSpeed(MAX_SPEED * percentSpeed);
+void jmoveToCoordinates(float x, float y, float z)
+{
+	moveToCoordinates(x, y, z);
+	stepper1.setMaxSpeed(MAX_SPEED *percentSpeed);
+	stepper2.setMaxSpeed(MAX_SPEED *percentSpeed);
+	stepper3.setMaxSpeed(MAX_SPEED *percentSpeed);
 }
 
-void arcmoveCC(float x, float y, float z, float r, float sAngle, float eAngle){
-  for(int angle = sAngle; angle < eAngle; angle += 1){
-    float calcXpos = (r * cos(radians(angle))) + x;
-    float calcYpos = (r * sin(radians(angle))) + y;
-    jmoveToCoordinates(calcXpos, calcYpos, z);
-    while(stepper1.distanceToGo() != 0 && stepper2.distanceToGo() != 0 && stepper3.distanceToGo() != 0){
-      stepper1.run();
-      stepper2.run();
-      stepper3.run();
-    }
-    delay(100);
-  }
-}
-void arcmoveC(float x, float y, float z, float r, float sAngle, float eAngle){
-  for(int angle = sAngle; angle > eAngle; angle -= 1){
-    float calcXpos = (r * cos(radians(angle))) + x;
-    float calcYpos = (r * sin(radians(angle))) + y;
-    jmoveToCoordinates(calcXpos, calcYpos, z);
-    while(stepper1.distanceToGo() != 0 && stepper2.distanceToGo() != 0 && stepper3.distanceToGo() != 0){
-      stepper1.run();
-      stepper2.run();
-      stepper3.run();
-    }
-    delay(100);
-  }
+void arcmoveCC(float x, float y, float z, float r, float sAngle, float eAngle)
+{
+	for (int angle = sAngle; angle < eAngle; angle += 1)
+	{
+		float calcXpos = (r* cos(radians(angle))) + x;
+		float calcYpos = (r* sin(radians(angle))) + y;
+		jmoveToCoordinates(calcXpos, calcYpos, z);
+		while (stepper1.distanceToGo() != 0 && stepper2.distanceToGo() != 0 && stepper3.distanceToGo() != 0)
+		{
+			stepper1.run();
+			stepper2.run();
+			stepper3.run();
+		}
+
+		delay(100);
+	}
 }
 
-void setMotionAccel(int accel){
-  stepper1.setAcceleration(accel);
-  stepper2.setAcceleration(accel);
-  stepper3.setAcceleration(accel);
+void arcmoveC(float x, float y, float z, float r, float sAngle, float eAngle)
+{
+	for (int angle = sAngle; angle > eAngle; angle -= 1)
+	{
+		float calcXpos = (r* cos(radians(angle))) + x;
+		float calcYpos = (r* sin(radians(angle))) + y;
+		jmoveToCoordinates(calcXpos, calcYpos, z);
+		while (stepper1.distanceToGo() != 0 && stepper2.distanceToGo() != 0 && stepper3.distanceToGo() != 0)
+		{
+			stepper1.run();
+			stepper2.run();
+			stepper3.run();
+		}
+
+		delay(100);
+	}
 }
 
-void reZero(){
-  stepper1.setSpeed(ZERO_SPEED);
-  stepper2.setSpeed(ZERO_SPEED);
-  stepper3.setSpeed(ZERO_SPEED);
-  while(digitalRead(stepper1endstop) == LOW || digitalRead(stepper2endstop) == LOW || digitalRead(stepper3endstop) == LOW){
-    if(digitalRead(stepper1endstop) == LOW){
-      stepper1.runSpeed();
-    }
-    if(digitalRead(stepper2endstop) == LOW){
-      stepper2.runSpeed();
-    }
-    if(digitalRead(stepper3endstop) == LOW){
-      stepper3.runSpeed();
-    }
-  }
+void setMotionAccel(int accel)
+{
+	stepper1.setAcceleration(accel);
+	stepper2.setAcceleration(accel);
+	stepper3.setAcceleration(accel);
+}
+
+void reZero()
+{
+	stepper1.setSpeed(ZERO_SPEED);
+	stepper2.setSpeed(ZERO_SPEED);
+	stepper3.setSpeed(ZERO_SPEED);
+	while (digitalRead(stepper1endstop) == LOW || digitalRead(stepper2endstop) == LOW || digitalRead(stepper3endstop) == LOW)
+	{
+		if (digitalRead(stepper1endstop) == LOW)
+		{
+			stepper1.runSpeed();
+		}
+
+		if (digitalRead(stepper2endstop) == LOW)
+		{
+			stepper2.runSpeed();
+		}
+
+		if (digitalRead(stepper3endstop) == LOW)
+		{
+			stepper3.runSpeed();
+		}
+	}
 }
